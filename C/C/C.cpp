ca72fbb25ac1507d1c23e6a86aa93495f0b47b7c
Oranven <2561860767@qq.com>
2023-08-31 20:15:10 +0800

    first commit

---
+++ C/C/C.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a, b, c;
+	
+	scanf_s("%d %d %d", &a, &b, &c);
+
+	double d = (a + b + c) / 3;
+
+	printf("这个三个整数%d %d %d 的平均数为%f", a, b, c, d);
+
+	return 0;
+}
